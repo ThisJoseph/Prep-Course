2123834fb9d3ce54a3a0db954f4ca9a31d73c3b6
ThisJoseph <montanajosemiguel78@gmail.com>
2022-11-10 13:34:44 -0500

    first homework completed

---
+++ 02-JS-I/homework/comprencio.js
@@ -0,0 +1,15 @@
+//VARIABLES: una variable es un termino utilizado para almazenar un valor en una palabra que nosotros queramos,
+// se invoca con el comando "var";
+
+// STRING: una String es un bloque de texto que siempre va a estar escritas entre comillas
+// y que puede estar escritas dentro de una variable.
+
+//FUNCIONES: una funcion es un objeto invocalbel que almacena una cantida determinada de variables
+//las cuales pieden ser almacenadas en una funcion, con la palbra clave "function", invocarlas, almacenarlas 
+//y hacer algo con ellas.
+
+//IF: is es una palabra clave que nos ayudara a verificar si un termino es "true", y en el caso
+//de que si lo sea, ejecutar el codigo suministrado, y en el caso de que no, omitir es codigo y avanzar.
+
+//VALORES BOOLEANOS: los booleanos son terminos que se untiliza para indicar que una variable puede ser
+//verdadera o falsa.
